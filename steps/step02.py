9c0163de957700ed5b1fcfa58c7e091325845d41
Yutaro <katoyutaro1122@icloud.com>
2025-04-03 20:19:36 +0900

    modify step02

---
+++ steps/step02.py
@@ -0,0 +1,24 @@
+import numpy as np
+from step01 import Variable
+
+class Function:
+    def __call__(self, input):
+        x = input.data
+        y = self.forward(x)
+        output = Variable(y)
+        return output
+    
+    # set function when using this object
+    def forward(self, x):
+        raise NotImplementedError()
+
+# example of how to use function class
+class Square(Function):
+    def forward(self, x):
+        return x ** 2
+    
+x = Variable(np.array(10))
+f = Square()
+y = f(x)
+print(type(y))
+print(y.data)
